f12234eea3aa0da44dff285ebc42a48e1a6743b1
João Rosa <joao.rosa@dazn.com>
2022-12-08 14:06:08 +0100

    readme

--- src/utils/build_md.py
+++ src/utils/build_md.py
@@ -13,8 +13,10 @@ years = os.listdir("src/years")
 content = []
 for year in years:
     content.append(f"* {parse(year)}")
-    solutions = filter(lambda x: ".py" in x and "init" not in x, os.listdir(f"src/years/{year}"))
-    content += [f"    *{parse(f'{solution}', f'{year}/')}" for _, solution in enumerate(sorted(solutions))]
+    solutions = filter(lambda x: ".py" in x and "init" not in x,
+                       os.listdir(f"src/years/{year}"))
+    content += [f"    *{parse(f'{solution}', f'{year}/')}" for _,
+                solution in enumerate(sorted(solutions))]
 
 readme_content = "# Advent of Code\n\n"
 readme_content += "## Usage\n\n## Problems list:\n"
